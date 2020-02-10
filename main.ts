3321549edb52565a5bdf9b047e18995bdbff8b03
dylan0000 <58610746+dylan0000@users.noreply.github.com>
2020-02-10 14:01:35 -0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,5 @@
 let Name = game.askForString("what is your name")
 let Color = game.askForString("what is you favorite color")
 let DogorCat = game.askForString("do you like dogs or cats")
-game.splash(" your name is: " + Name)
-game.splash(" favorite color : " + Color)
-game.splash(" you like: " + DogorCat)
+game.splash(" your name is: " + Name + " favorite color : " + Color + " you like: " + DogorCat)
 game.reset()
